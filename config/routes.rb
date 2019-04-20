8f5952c8f2622adbe6a674aa1058427037921a7c
Kapil <kapilgoyal6@gmail.com>
2019-04-20 19:23:13 +0530

    modified ui

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'welcome/index'
-  root 'welcome#index'
+  root 'articles#index'
   resources :articles
   resources :blogs
 	  resources :articles do
